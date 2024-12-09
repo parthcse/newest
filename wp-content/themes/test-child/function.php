c1ef714caed6ee548f4041c70db5cb782d4eccaf
parthcse <parthpatelj@gmail.com>
2024-12-09 16:41:16 +0530

    Child Theme

---
+++ wp-content/themes/test-child/function.php
@@ -0,0 +1,14 @@
+<?php
+// Enqueue the parent theme's styles
+function twentytwentyfive_child_enqueue_styles() {
+    wp_enqueue_style(
+        'twentytwentyfive-parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+    wp_enqueue_style(
+        'twentytwentyfive-child-style',
+        get_stylesheet_uri(),
+        array('twentytwentyfive-parent-style')
+    );
+}
+add_action('wp_enqueue_scripts', 'twentytwentyfive_child_enqueue_styles');
